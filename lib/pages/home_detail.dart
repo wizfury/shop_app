31df298ba20dab5edb37cf074af759650ff7d5ce
wizfury <Aakarsh135@gmail.com>
2023-01-22 00:36:35 +0530

    homepage

--- lib/pages/home_detail.dart
+++ lib/pages/home_detail.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/models/catalog.dart';
+import 'package:shop_app/utils/routes.dart';
 import 'package:shop_app/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class HomeDetailPage extends StatelessWidget {
   final Item catalog;
-  const HomeDetailPage({super.key, required this.catalog}) : assert(catalog!=null);
-  
+  const HomeDetailPage({super.key, required this.catalog})
+      : assert(catalog != null);
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +19,10 @@ class HomeDetailPage extends StatelessWidget {
         children: [
           "\$${catalog.price}".text.color(Colors.red).bold.xl2.make(),
           ElevatedButton(
-            onPressed: () {},
-            child: "+ cart".text.make(),
+            onPressed: () {
+              Navigator.pushNamed(context, MyRoutes.orderRoute);
+            },
+            child: "Buy".text.make(),
             style: ButtonStyle(
                 shape: MaterialStateProperty.all(StadiumBorder()),
                 backgroundColor:
@@ -28,37 +31,31 @@ class HomeDetailPage extends StatelessWidget {
         ],
       ).p16(),
       backgroundColor: Colors.white,
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-      
-          children: [
-          Container(
-            
-            child: Hero(
-              tag: Key(catalog.id.toString()),
-              child: Image.network(catalog.image)).p16()).centered().expand(),
-      
-          Expanded(
+      body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Container(
+                child: Hero(
+                        tag: Key(catalog.id.toString()),
+                        child: Image.network(catalog.image))
+                    .p16())
+            .centered()
+            .expand(),
+        Expanded(
             child: VxArc(
-              height: 20.0,
-              arcType: VxArcType.CONVEY,
-              edge: VxEdge.TOP,
-              
-              child: Container(
-                color: MyTheme.creamcolor,
-                width: context.screenWidth,
-                child: Column(children: [
-                  catalog.name.text.xl4.color(MyTheme.darkbluishcolor).bold.make(),
-      
-                  catalog.desc.text.sm.color(MyTheme.darkbluishcolor).make()
-                ]).p64(),
-                
+                height: 20.0,
+                arcType: VxArcType.CONVEY,
+                edge: VxEdge.TOP,
+                child: Container(
+                  color: MyTheme.creamcolor,
+                  width: context.screenWidth,
+                  child: Column(children: [
+                    catalog.name.text.xl4
+                        .color(MyTheme.darkbluishcolor)
+                        .bold
+                        .make(),
+                    catalog.desc.text.sm.color(MyTheme.darkbluishcolor).make()
+                  ]).p64(),
                 )))
-        
-        ]),
+      ]),
     );
-
-      
-    
   }
 }
